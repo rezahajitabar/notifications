261b7c1b62f2d7d6f0595be3fee6565b3a68824f
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-03-16 10:55:11 +0330

    third commit project

--- src/component/Navbars/Navbar.js
+++ src/component/Navbars/Navbar.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
 import './Navbar.css'
 export default class Navbar extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+   Notification:[ 
+  "1,اطلاعیه شماره",
+   "2,اطلاعیه شماره",
+   "3,اطلاعیه شماره",
+   "4,اطلاعیه شماره",
+   "5,اطلاعیه شماره"]  
+    }
+  }
+
+  
   render() {
     return (
         <nav>
-          <div className='container'>Navbar</div>
+          <div className='container'>
+            <h1>
+              Hello World
+            </h1>
+            <div className='container-main'>
+             <h4>News</h4>
+             {this.state.Notification.length >= 5 && 
+             <span>123</span>
+             }
+          </div>
+          </div>
+          
+          
         </nav>
      
     )
